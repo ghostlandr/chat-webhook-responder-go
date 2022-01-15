aeb167c905c52639cf1eb299c17883c204baec50
Graham <8973794+ghostlandr@users.noreply.github.com>
2022-01-15 15:58:13 -0600

    test: switch to table tests for Term

--- go-app/internal/term/term_test.go
+++ go-app/internal/term/term_test.go
@@ -6,23 +6,61 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRawSplitsOnColon(t *testing.T) {
-	ter := Term("define: butt")
-	want := "butt"
-	got := ter.Raw()
-	assert.Equal(t, want, got, "Raw() should split on colon")
-}
-
-func TestRawReturnsOriginalStringIfNoColon(t *testing.T) {
-	ter := Term("butt")
-	want := "butt"
-	got := ter.Raw()
-	assert.Equal(t, want, got, "Raw() returns original string if no colon present")
-}
-
 func TestStringJoinsOnHyphen(t *testing.T) {
 	ter := Term("define: just long things")
 	want := "just-long-things"
 	got := ter.String()
 	assert.Equal(t, want, got, "String() should join term on hyphens")
+}
+
+func TestString(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+		term string
+	}{
+		{
+			name: "String() should slugify with hyphens",
+			want: "just-long-things",
+			term: "define: just long things",
+		},
+		{
+			name: "String() should slugify even without a colon-delimited command",
+			want: "more-long-things",
+			term: "More long things",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got := Term(tc.term).String()
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func TestRawTerms(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+		term string
+	}{
+		{
+			name: "Raw() should split on colon",
+			want: "butt",
+			term: "define: butt",
+		},
+		{
+			name: "Raw() returns original string if no colon present",
+			want: "butt",
+			term: "butt",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := Term(test.term).Raw()
+			assert.Equal(t, test.want, got, test.name)
+		})
+	}
 }
