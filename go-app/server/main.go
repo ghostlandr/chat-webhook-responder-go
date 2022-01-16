ea4c719223c51b3f45878e45f60b6cb0c75d0750
Graham <8973794+ghostlandr@users.noreply.github.com>
2022-01-15 20:20:39 -0600

    tidy: move middlewares to middleware.go

--- go-app/server/main.go
+++ go-app/server/main.go
@@ -33,45 +33,12 @@ func main() {
 	logger := client.Logger("chat-webhook-responder-go").StandardLogger(logging.Info)
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/define", checkVerification(definer.New(logger).ServeDefinerRequest, define))
-	mux.HandleFunc("/udefine", checkVerification(udefiner.New(logger).ServeUrbanDefinerRequest, udefine))
+	mux.HandleFunc("/define", addContext(ctx, checkVerification(definer.New(logger).ServeDefinerRequest, define)))
+	mux.HandleFunc("/udefine", addContext(ctx, checkVerification(udefiner.New(logger).ServeUrbanDefinerRequest, udefine)))
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 	log.Printf("Listening on port %s", port)
 	log.Printf("Error? %s", http.ListenAndServe(fmt.Sprintf(":%s", port), mux))
-}
-
-func checkVerification(fn http.HandlerFunc, definerType definers) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		// var signingSecret string
-		// switch definerType {
-		// case define:
-		// 	signingSecret = definesigningSecret
-		// case udefine:
-		// 	signingSecret = udefineSigningSecret
-		// }
-		// verifier, err := slack.NewSecretsVerifier(r.Header, signingSecret)
-		// if err != nil {
-		// 	w.WriteHeader(http.StatusInternalServerError)
-		// 	return
-		// }
-
-		// if err = verifier.Ensure(); err != nil {
-		// 	log.Printf("verification failed, but letting things through for now in testing phase, %v", err)
-		// 	// w.WriteHeader(http.StatusUnauthorized)
-		// 	// return
-		// }
-
-		// If all is well, pass the request along
-		fn(w, r)
-	}
-}
-
-func addContext(ctx context.Context, fn http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		req := r.WithContext(ctx)
-		fn(w, req)
-	}
 }
