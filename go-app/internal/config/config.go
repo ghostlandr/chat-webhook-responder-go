766794cc881b173f9008714a3f3777f40b338832
Graham <8973794+ghostlandr@users.noreply.github.com>
2022-01-29 07:49:02 -0600

    config: use env vars rather than secret file

--- go-app/internal/config/config.go
+++ go-app/internal/config/config.go
@@ -1,6 +1,13 @@
 package config
 
+import "os"
+
 var (
 	DefinerClientID  = "11970859830.2903858978897"
 	UdefinerClientID = "11970859830.2902453792341"
 )
+
+var DefinerClientSecret string = os.Getenv("DEFINERCLIENTSECRET")
+var DefineSigningSecret string = os.Getenv("DEFINESIGNINGSECRET")
+var UdefinerClientSecret string = os.Getenv("UDEFINERCLIENTSECRET")
+var UdefineSigningSecret string = os.Getenv("UDEFINESIGNINGSECRET")
